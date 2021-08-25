eb352926cc48fbae00634bcfb56d89323df08030
ysferdgnn <ysferdgnn@gmail.com>
2021-08-25 09:37:11 +0300

    SpreadSheetFormat enum class created

---
+++ src/main/java/com/seras/enums/SpreadSheetFormat.java
@@ -0,0 +1,13 @@
+package com.seras.enums;
+
+
+/**
+ * @author Yusuf Erdoğan
+ * @version 1.0
+ * HSSF : Horrible SpreadSheet Format use '.xls' files.
+ * XSSF : XML SpreadSheet Format use '.xlsx' files.
+* */
+public enum SpreadSheetFormat {
+    HSSF,
+    XSSF
+}
