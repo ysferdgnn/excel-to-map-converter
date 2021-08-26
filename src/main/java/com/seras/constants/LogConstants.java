7c5075adfc9a18bb8c93c70b18a788505ddfc032
ysferdgnn <ysferdgnn@gmail.com>
2021-08-26 17:56:55 +0300

    add new log constants

---
+++ src/main/java/com/seras/constants/LogConstants.java
@@ -0,0 +1,17 @@
+package com.seras.constants;
+
+public class LogConstants {
+
+    private static LogConstants logConstants;
+    private LogConstants(){};
+    public static  LogConstants getInstance(){
+        if (logConstants==null)
+            logConstants=new LogConstants();
+        return logConstants;
+    }
+
+    public String msgRowEmpty ="Row is empty. Return null";
+    public String msgHeaderListEmpty ="HeaderList is empty. Return null";
+    public String msgRowListEmpty ="Row list is empty. Return null";
+    public String msgSheetEmty="Sheet is empty.Return null";
+}
